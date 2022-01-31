b18784faf8bb11c9b0ec18123b38c72281cd9378
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-01-31 00:02:42 -0500

    Striving to reinforce  my fundamentals

---
+++ dataTypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+int x = 5;
+int y = -3;
+int z;
+cout<< "Type in a number: "<<endl;
+cin>> z;
+cout<< "Your number cubed is: "<< z*z*z<<endl;
+//cout<<(double)x / y;
+
+
+double a = .0003, b =78453, c = -143.234;
+double d = 1.13e13;
+double f = 3.14e-7;
+
+bool test = true;
+long g = 100000;
+float h = 3.14159f;
+char letter = 'a';
+string greet = "How are you?";
+
+}
